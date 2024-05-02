4ce8223319064056f1556c517a5b5760789f5587
Faesalkhan <pathanf9545@gmail.com>
2024-05-02 15:44:12 +0530

    add editing the note then save it, and searching the note

--- src/App.js
+++ src/App.js
@@ -2,39 +2,86 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import NoteList from "./components/NoteList";
+
 const App = () => {
+  const today = new Date();
   const [notes, setNotes] = useState([]);
-  const [allnotes, setAllNotes] = useState([]);
-  const [darkmode, setDarkmode] = useState(false);
-  const addNote = (xnotetext) => {
-    const dt = new Date();
+  const [allNotes, setAllNotes] = useState([]);
+  const addingNote = (xnotetext) => {
     const newNote = {
       id: Date.now(),
       text: xnotetext,
-      date: dt.toLocaleDateString(),
+      date: today.toLocaleDateString(),
     };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
+    const notesAll = [...notes, newNote];
+    setNotes(notesAll);
+    setAllNotes(notesAll);
   };
-  const deleteNote = (xid) => {
-    const updatednotes = notes.filter((note) => note.id !== xid);
-    setNotes(updatednotes) || setAllNotes(updatednotes);
+  const deletingNote = (xnote) => {
+    const updatedNotes = notes.filter((note) => note.id !== xnote.id);
+    setNotes(updatedNotes);
+    setAllNotes(updatedNotes);
   };
-  const updateSearch = (searching) => {
-    const updated = notes.filter((note) => note.text.includes(searching));
-    setAllNotes(updated);
+  const [editId, setEditid] = useState(null);
+  const [editNote, setEditnote] = useState({
+    id: "",
+    text: "",
+    date: "",
+  });
+  const editingNote = (xnote) => {
+    setEditid(xnote.id);
+    const noteVals = {
+      id: xnote.id,
+      text: xnote.text,
+      date: xnote.date,
+    };
+    setEditnote(noteVals);
   };
 
+  const saveEditNote = () => {
+    const editedNote = {
+      id: editId,
+      text: editNote.text,
+      date: editNote.date,
+    };
+    const updated = notes.map((note) => {
+      if (note.id === editId) {
+        return editedNote;
+      }
+      return note;
+    });
+    setNotes(updated);
+    setAllNotes(updated);
+    setEditid(null);
+  };
+  const [search, setSearch] = useState("");
+  const updateSearch = (searching) => {
+    const filtered = notes.filter((note) => note.text.includes(searching));
+    setAllNotes(filtered);
+  };
+  const [darkmode, setDarkmode] = useState(false);
+
   return (
-    <div className={`${darkmode && "dark-mode"} `}>
-      <div className="container appContainer ">
+    <div className={`${darkmode && "dark-mode"}`}>
+      <div className="appContainer">
         <Header darkmode={darkmode} setDarkmode={setDarkmode} />
-        <Search updateSearch={updateSearch} />
+        <Search
+          search={search}
+          setSearch={setSearch}
+          updateSearch={updateSearch}
+        />
         <NoteList
           notes={notes}
-          allnotes={allnotes}
-          addNote={addNote}
-          deleteNote={deleteNote}
+          addingNote={addingNote}
+          deletingNote={deletingNote}
+          editingNote={editingNote}
+          editId={editId}
+          setEditid={setEditid}
+          saveEditNote={saveEditNote}
+          editNote={editNote}
+          setEditnote={setEditnote}
+          allNotes={allNotes}
+          setAllNotes={setAllNotes}
         />
       </div>
     </div>
